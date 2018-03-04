b58cf4278829cb6fd5a7ff0cabdf6c3ac9d56626
ivanlyah <ivan.lyah@foxtrapp.com>
2018-03-04 12:15:32 +0200

    feat(*): add vr tour skeleton

---
+++ app/js/initialize.js
@@ -0,0 +1,11 @@
+require('aframe-look-at-component');
+// require('platform');
+// require('aframe-always-fullscreen-component');
+require('./components/tour');
+require('./components/panorama');
+require('./components/hotspot');
+require('./components/helper');
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Initialized app');
+});
